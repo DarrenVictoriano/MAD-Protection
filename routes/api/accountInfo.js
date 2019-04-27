132f0eb413875eefbe575609eecc108ff61e9421
Darren Victoriano <darren.victoriano@gmail.com>
2019-04-27 11:37:35 -0700

    added boilerplate for routes

---
+++ routes/api/accountInfo.js
@@ -0,0 +1,1 @@
+// do routes for account info
---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const userInfoRouter = require("./userInfo");
+const accountInfoRouter = require("./accountInfo");
+
+router.use("/user", userInfoRouter);
+router.use("/account", accountInfoRouter);
+
+module.exports = router;
+
