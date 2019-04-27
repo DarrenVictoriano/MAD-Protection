c957a49f89d72040fb59e5746eaaf88a6d9d172b
Darren Victoriano <darren.victoriano@gmail.com>
2019-04-27 13:14:50 -0700

    updated findAll

--- controllers/userInfoController.js
+++ controllers/userInfoController.js
@@ -4,9 +4,18 @@ const mad = require("./encryption");
 module.exports = {
     findAll: function (req, res) {
         db.UserInfo
-            .find(req.query)
+            .find()
             .sort({ lastName: 1 })
-            .then(dbUserInfo => res.json(dbUserInfo))
+            .then(dbUserInfo => {
+                let decrptedData = {
+                    _id: dbUserInfo._id,
+                    firstName: mad.decrypt(dbUserInfo.firstName),
+                    lastName: mad.decrypt(dbUserInfo.lastName),
+                    email: mad.decrypt(dbUserInfo.email),
+                    Password: mad.decrypt(dbUserInfo.Password)
+                }
+                res.json(decrptedData)
+            })
             .catch(err => res.status(422).json(err));
     },
     findById: function (req, res) {
@@ -15,10 +24,10 @@ module.exports = {
             .then(dbUserInfo => res.json(dbUserInfo))
             .catch(err => res.status(422).json(err));
     },
-    create: function(req, res) {
+    create: function (req, res) {
         console.log(typeof req.body.Password);
         encrytedData = {
-            firstName : mad.encrypt(req.body.firstName).toString(),
+            firstName: mad.encrypt(req.body.firstName).toString(),
             lastName: mad.encrypt(req.body.lastName).toString(),
             email: mad.encrypt(req.body.email).toString(),
             Password: mad.encrypt(req.body.Password).toString()
