3844446ebfa1bcb003cd68a0b3531aae7e664cd1
Darren Victoriano <darren.victoriano@gmail.com>
2019-04-27 21:08:07 -0700

    fixed findAll

--- controllers/userInfoController.js
+++ controllers/userInfoController.js
@@ -7,23 +7,20 @@ module.exports = {
             .find(req.query)
             .sort({ lastName: 1 })
             .then(dbUserInfo => {
+                console.log("dbUser: ", dbUserInfo);
                 let decrptedData = [];
 
                 dbUserInfo.forEach(item => {
                     decrptedData.push({
                         _id: item._id,
-                        firstName: item.firstName,
-                        lastName: item.lastName,
-                        email:item.email,
-                        Password: item.Password
+                        firstName: mad.decrypt(item.firstName),
+                        lastName: mad.decrypt(item.lastName),
+                        email: mad.decrypt(item.email),
+                        Password: mad.decrypt(item.Password)
                     });
                 });
 
-              
-                console.log("asdasd");
-                console.log("DECRYPTED " + decrptedData);
-                console.log(typeof decrptedData[0].firstName);
-
+                console.log("decrypt: ", decrptedData);
                 res.json(decrptedData)
             })
             .catch(err => res.status(422).json(err));
@@ -46,8 +43,8 @@ module.exports = {
             .catch(err => res.status(422).json(err));
     },
     create: function (req, res) {
-        console.log(typeof req.body.Password);
-        encrytedData = {
+
+        let encrytedData = {
             firstName: mad.encrypt(req.body.firstName).toString(),
             lastName: mad.encrypt(req.body.lastName).toString(),
             email: mad.encrypt(req.body.email).toString(),
@@ -60,15 +57,24 @@ module.exports = {
             .catch(err => res.status(422).json(err));
     },
     update: function (req, res) {
+
+        let encrytedData = {
+            firstName: mad.encrypt(req.body.firstName).toString(),
+            lastName: mad.encrypt(req.body.lastName).toString(),
+            email: mad.encrypt(req.body.email).toString(),
+            Password: mad.encrypt(req.body.Password).toString()
+        };
+
         db.UserInfo
-            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .findOneAndUpdate({ _id: req.params.id }, encrytedData)
             .then(dbUserInfo => res.json(dbUserInfo))
             .catch(err => res.status(422).json(err));
     },
     remove: function (req, res) {
         db.UserInfo
             .findById({ _id: req.params.id })
-            .then(dbUserInfo => res.json(dbUserInfo))
+            .then(dbUserInfo => dbUserInfo.remove())
+            .then(removedDB => res.json(removedDB))
             .catch(err => res.status(422).json(err));
     }
 };
