b2eda1942382cff8d93f5636555b853bde930da9
Darren Victoriano <darren.victoriano@gmail.com>
2019-05-08 21:18:50 -0700

    updated with comments

--- routes/api/accountInfo.js
+++ routes/api/accountInfo.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const accountInfoController = require("../../controllers/acctInfoController");
 
 router.route("/")
-    .get(accountInfoController.findAll);
+    .get(accountInfoController.findAll); // admin protected
 
 router.route("/:id")
-    .post(accountInfoController.create)
-    .get(accountInfoController.findbyID)
-    .put(accountInfoController.update)
-    .delete(accountInfoController.remove);
+    .post(accountInfoController.create) // protected
+    .get(accountInfoController.findbyID) // protected
+    .put(accountInfoController.update) // protected
+    .delete(accountInfoController.remove); // protected
 
 module.exports = router;
